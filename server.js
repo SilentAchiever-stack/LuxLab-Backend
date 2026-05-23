39e6767f33fd8d677c72f02074e55d6c13724331
keji <olatunjisarahmorenikeji@gmail.com>
2026-05-23 21:07:47 +0100

    initial commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://react-luxlab.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true
 }));
